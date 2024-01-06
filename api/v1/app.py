18694d1f92b2786ddc46a7c9f9e73dd95a43aa24
Boscomunich <solomon4chidera@gmail.com>
2024-01-06 15:58:40 +0000

    addederror handling in task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+::/usr/bin/python3
 '''a flask web api.
 '''
 import os
@@ -22,6 +22,20 @@ def teardown_flask(exception):
     '''The Flask app/request context end event listener.'''
     # print(exception)
     storage.close()
+
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,4 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 '''The blueprint for the AirBnB clone API.'''
 
-from .index import *
+from api.v1.views.index import *
